2d5cc3148d7051c30cdbdaa315b310dff4fe18c0
Klimchuk-sergei <klim.work@mail.ru>
2025-12-23 17:53:28 +0300

    create point model

--- point/models.py
+++ point/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Point(models.Model):
+    """Модель точек на карте"""
+    name  = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    location = models.PointField(geography=True, srid=4326)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='points')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        index = [models.Index(fields=['location'])]
+
+    def __str__(self):
+        return self.name
+
+class
+
+
